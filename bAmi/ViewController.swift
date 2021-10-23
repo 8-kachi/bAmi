290d216b943c33eba8c41e940de5e33f13db389a
8_kachi_ <ponta510000@gmail.com>
2021-10-23 21:19:44 +0900

    Initial Commit

---
+++ bAmi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bAmi
+//
+//  Created by 浅野総一郎 on 2021/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
